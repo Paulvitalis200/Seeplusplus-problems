e175ad52a58a6bd4d863a86732141aa3aed7252d
Paul Otieno <vitalispaul48@live.com>
2026-03-30 05:14:03 +0300

    Add Transform checks

--- main.cpp
+++ main.cpp
@@ -16,10 +16,10 @@ int getInputValue() {
 }
 
 
-bool isLeapYear(int year) {
-    if (year % 4 == 0)
-        return true;
-    return false;
+std::string isLeapYear(const int year) {
+    if (year % 4 == 0 && year != 0)
+        return "Leap Year!";
+    return "Not a leap year";
 }
 
 bool checkLeapYearFn() {
@@ -29,14 +29,16 @@ bool checkLeapYearFn() {
     std::cout << "Would you like to check another year? (y/n) ";
 
     std::string response;
-
+    std::transform(response.begin(), response.end(), response.begin(), ::tolower);
     std::cin >> response;
+
     while (response != "y" &&
         response != "yes" &&
         response != "n" &&
         response != "no"
     ) {
         std::cout << "Please enter a valid choice. (y is yes, n is no) ";
+        std::transform(response.begin(), response.end(), response.begin(), ::tolower);
         std::cin >> response;
     }
 
